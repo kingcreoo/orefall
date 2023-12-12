469031463b3ef9a99d3e29b32d310c96c6628bb5
Creoo <151230589+kingcreoo@users.noreply.github.com>
2023-12-12 13:56:12 -0500

    Add selene, settings & write to client, server
    -Init selene(linter)
    -Add settings module
    -Add basics to client & server

---
+++ src/server/Settings.lua
@@ -0,0 +1,4 @@
+local Settings = {}
+
+
+return Settings
--- src/server/init.server.lua
+++ src/server/init.server.lua
@@ -1,1 +1,30 @@
 -- / / Server, created by KingCreoo on 12/11/23
+
+-- / / SERVICES
+
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local ServerScriptService = game:GetService("ServerScriptService")
+
+-- / / MODULES
+
+local _Settings = require(ServerScriptService:WaitForChild("Server"):WaitForChild("Settings"))
+
+-- / / VARIABLES
+
+local Events: Folder = ReplicatedStorage:WaitForChild("Events")
+
+local LoadEvent: RemoteEvent = Events:WaitForChild("Load")
+
+-- / / FUNCTIONS
+
+local function LoadPlayer(Player: Player)
+
+    LoadEvent:FireClient(Player) -- Communicate to the client that player has been loaded.
+end
+
+-- / / REMOTES
+
+-- / / EVENTS
+
+Players.PlayerAdded:Connect(LoadPlayer)
