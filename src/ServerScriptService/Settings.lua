28f93cddb9a4dd30d63e1994ceae55bb274ae468
Creoo <151230589+kingcreoo@users.noreply.github.com>
2023-12-12 20:50:05 -0500

    Add DataStore :)
    -add module-based playerdata database (no hackermen)
    -add basic datastore saves
    -add default data for new players
    -add deepreconcile to make sure players' data is up to date with the newest version of the game

--- src/ServerScriptService/Settings.lua
+++ src/ServerScriptService/Settings.lua
@@ -7,4 +7,11 @@ Settings.Locations = {
     [2] = {["CFrame"] = CFrame.new(-40,0,0), ["Occupied"] = false},
 }
 
+Settings.DefaultData = { -- In the future we will have to write a system that creates default data based off of the game's files.
+    ["leaderstats"] = {
+        ["Cash"] = 100
+    },
+    ["Version"] = Settings.Version
+}
+
 return Settings
--- src/ServerScriptService/init.server.lua
+++ src/ServerScriptService/init.server.lua
@@ -6,10 +6,14 @@ local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 local ServerScriptService = game:GetService("ServerScriptService")
 
+local DataStoreService = game:GetService("DataStoreService")
+local DataStore = DataStoreService:GetDataStore("DataStore")
+
 -- / / MODULES
 
 local _Settings = require(ServerScriptService:WaitForChild("Server"):WaitForChild("Settings"))
 local _Tower = require(ServerScriptService:WaitForChild("Server"):WaitForChild("Tower"))
+local _Data = require(ServerScriptService:WaitForChild("Server"):WaitForChild("Data"))
 
 -- / / VARIABLES
 
@@ -20,9 +24,60 @@ local LoadEvent: RemoteEvent = Events:WaitForChild("Load")
 
 -- / / FUNCTIONS
 
+local function DeepCopy(Table)
+    local Copy = {}
+
+    for k, v in pairs(Table) do
+        if type(v) == table then
+            DeepCopy(v)
+        else
+            Copy[k] = v
+        end
+    end
+
+    return Copy
+end
+
+local function DeepReconcile(Table0 --[[Default data]], Table1 --[[Player's data]])
+    for k, v in pairs(Table0) do
+        if not Table1[k] then continue end
+
+        if type(v) == table then
+            DeepReconcile(Table0[k], Table1[k])
+        else
+            if Table1[k] then 
+                Table0[k] = Table1[k] 
+            end
+        end
+    end
+end
+
 local function LoadPlayer(Player: Player)
     local Character = Player.Character or Player.CharacterAdded:Wait()
     Character:SetPrimaryPartCFrame(workspace:WaitForChild("Limbo").CFrame + Vector3.new(0,5,0)) -- Teleport player to limbo
+
+    local PlayerData = DataStore:GetAsync("Player_" .. Players:GetUserIdFromNameAsync(Player.Name))
+    if not PlayerData then
+        print(Player.Name .. " is a new player.")
+        PlayerData = _Data:NewPlayer(Player)
+    else
+        if PlayerData["Version"] ~= _Settings.Version then
+            DeepReconcile(_Settings.DefaultData, PlayerData) 
+        end
+    end
+
+    _Data.Set(Player, PlayerData)
+
+    local leaderstats = Instance.new("Folder")
+    leaderstats.Name = "leaderstats"
+    leaderstats.Parent = Player
+
+    for Stat, Value in pairs(PlayerData["leaderstats"]) do
+        local NewValue = Instance.new("IntValue")
+        NewValue.Name = Stat
+        NewValue.Value = Value
+        NewValue.Parent = leaderstats
+    end
 
     local PlayerTower = _Tower.New(Player)
     PlayerTower:Add() -- Create a tower for the player and assign a vacant(now occupied) location.
@@ -36,6 +91,9 @@ end
 local function RemovePlayer(Player: Player)
     local PlayerTower = Towers[Player.Name]
     PlayerTower:Remove() -- Remove the player's tower from the workspace and mark it's location as vacant.
+
+    local PlayerData = _Data.Remove(Player) -- Remove player's data from database (and save the data.)
+    -- Save the player's data here, just in case.
 end
 
 -- / / REMOTES
